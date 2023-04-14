f95b4b50c914435892f416c04d11252498e367f7
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-04-14 13:18:41 +0300

    table creation

--- NodeRest/database.sql
+++ NodeRest/database.sql
@@ -1,12 +1,24 @@
-create database NokiaHqEmotions;
+create DATABASE NokiaHqEmotions;
 
-use NokiaHqEmotions
+USE NokiaHqEmotions
 
-create table emotions(
-    id int auto_increment,
-    emotion_id int not null,
-    created_at datetime default current_timestamp,
-    primary key(id)
+CREATE TABLE emotions(
+    id INT auto_increment,
+    emotion_id INT NOT NULL,
+    sub_emotion_id INT NOT NULL,
+    created_at DATETIME DEFAULT current_timestamp,
+    PRIMARY KEY(id)
 );
 
-insert into emotions (emotion_id, created_at) VALUES ('1', '2023.03.23 14:57');
+INSERT INTO emotions (emotion_id, created_at) VALUES ('1', '2023.03.23 14:57');
+
+-- to drop table use 
+DROP emotions
+
+-- viimeksi korjasin bäkin subemotioneita varten, sain build toiminnon valmiiksi cicd fronttia varten, valmistelin cicd bäkkiä varten, tein vähäsen unit testejä, 
+-- tänään jaan viisauteni cicd suhteen ryhmän kanssa ja katsotaan että kaikki osaavat tehdä sen, opiskelen lisää ja implementoin unit testejä, siivoan ja kommentoin koodia.
+-- ei impedimentsejä  
+
+-- viimeksi tein cicd bäkille ryhmän kanssa, tain vähäsen unit testejä sekä tein uita statistiikkanäkymää varten.
+-- tänään valmistellen esitelmää sekä osallistun projektiesittelyyn ja tuotetestaukseen, aloitan statistiikkanäkymän koodausta 
+-- ei impedimentsejä
