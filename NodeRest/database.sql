dc051b57ad4363461d2721c4834ad2f93c3cfe5c
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-04-14 14:25:32 +0300

    table creation

--- NodeRest/database.sql
+++ NodeRest/database.sql
@@ -10,15 +10,8 @@ CREATE TABLE emotions(
     PRIMARY KEY(id)
 );
 
-INSERT INTO emotions (emotion_id, created_at) VALUES ('1', '2023.03.23 14:57');
+INSERT INTO emotions (emotion_id, sub_emotion_id, created_at) VALUES ('1', '2', '2023.03.23 14:57');
 
 -- to drop table use 
 DROP emotions
 
--- viimeksi korjasin bäkin subemotioneita varten, sain build toiminnon valmiiksi cicd fronttia varten, valmistelin cicd bäkkiä varten, tein vähäsen unit testejä, 
--- tänään jaan viisauteni cicd suhteen ryhmän kanssa ja katsotaan että kaikki osaavat tehdä sen, opiskelen lisää ja implementoin unit testejä, siivoan ja kommentoin koodia.
--- ei impedimentsejä  
-
--- viimeksi tein cicd bäkille ryhmän kanssa, tain vähäsen unit testejä sekä tein uita statistiikkanäkymää varten.
--- tänään valmistellen esitelmää sekä osallistun projektiesittelyyn ja tuotetestaukseen, aloitan statistiikkanäkymän koodausta 
--- ei impedimentsejä
