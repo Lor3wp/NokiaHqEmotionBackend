120ba9f99cb3ce0e46686bc40af6d150bb683e3b
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-04-14 13:17:42 +0300

    addemotion updated

--- NodeRest/routes/addEmotion.js
+++ NodeRest/routes/addEmotion.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 router.post("/addemotion", async function (req, res) {
   try {
-    const { emotion } = req.body;
-    const sqlQuery = "INSERT INTO emotions (emotion_id) VALUES (?)";
-    const result = await pool.query(sqlQuery, [emotion]);
+    const { emotion, subEmotion } = req.body;
+    console.log(emotion + " " + subEmotion);
+    const sqlQuery =
+      "INSERT INTO emotions (emotion_id, sub_emotion_id) VALUES (?, ?)";
+    const result = await pool.query(sqlQuery, [emotion, subEmotion]);
     console.log(req.body);
     res.status(200).json({ emotionId: result.insertId.toString() });
   } catch (error) {
