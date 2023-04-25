f5c97394f377ddd00eed2eb0ad70611fa353c9c3
vilimaki <vilimaki@metropolia.fi>
2023-04-25 14:31:18 +0300

    queries for front view stats fixed

--- NodeRest/routes/emotionsRoute.js
+++ NodeRest/routes/emotionsRoute.js
@@ -289,4 +289,38 @@ router.post("/addemotion", async function (req, res) {
     }
 });
 
+router.get("/getallemotions", async function (req, res) {
+    try {
+        const sqlQuery = "select count(*) as count from emotions";
+        const rows = await pool.query(sqlQuery);
+        console.log(rows); // add this line to see what the query is returning
+        const count = Number(rows[0].count); // extract count value and convert to number
+        console.log(count); // add this line to see what the count value is
+        res.status(200).json({ count: count }); // send count value as a JSON object
+    } catch (error) {
+        console.log(error); // add this line to see what error message is being returned
+        res.status(400).json({ error: error.message }); // send error message as a JSON object
+    }
+});
+
+router.get("/gettodayemotions", async function (req, res) {
+    let date_ob = new Date();
+    let day = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+
+    let date = year + "-" + month + "-" + day;
+    console.log((date_ob.getTime() / 60) * 1000);
+    try {
+        const sqlQuery = `SELECT count(*) as count from emotions e where cast(e.created_at as date) = '${date}'`;
+        const rows = await pool.query(sqlQuery);
+        console.log(rows); // add this line to see what the count value is
+        const count = Number(rows[0].count); // extract count value and convert to number
+        res.status(200).json({ count: count }); // send count value as a JSON object
+    } catch (error) {
+        console.log(error); // add this line to see what error message is being returned
+        res.status(400).json({ error: error.message }); // send error message as a JSON object
+    }
+});
+
 module.exports = router;
