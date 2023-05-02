6318e65515db47f828f74125578e4bec946b5e37
vilimaki <vilimaki@metropolia.fi>
2023-05-02 11:51:49 +0300

    sqlite week works

--- NodeRest/routes/emotionsRoute.js
+++ NodeRest/routes/emotionsRoute.js
@@ -33,18 +33,20 @@ GROUP BY strftime('%H', created_at), emotion_id, sub_emotion_id;`;
   }
 });
 // [x]
-router.get("/getyear/:year", async function (req, res) {
+
+router.get("/getweek/:startdate/:enddate", async function (req, res) {
   try {
-    const year = req.params.year;
-    const sqlQuery =
-      "SELECT strftime('%m', created_at) AS created_at,\n" +
-      "emotion_id,\n" +
-      "sub_emotion_id,\n" +
-      "COUNT(*) AS count\n" +
-      "FROM emotions\n" +
-      "WHERE CAST(strftime('%Y', created_at)as INTEGER) = ?\n" +
-      "GROUP BY strftime('%m', created_at), emotion_id, sub_emotion_id;";
-    const params = [year];
+    console.log(req.params.startdate, req.params.enddate);
+
+    const sqlQuery = `SELECT strftime('%d', created_at) AS created_at,
+        emotion_id,
+        sub_emotion_id,
+        COUNT(*) AS count,
+        DATE(created_at) AS full_date
+        FROM emotions
+        WHERE DATE(created_at) BETWEEN ? AND ?\n
+        GROUP BY strftime('%d', created_at), emotion_id, sub_emotion_id;`;
+    const params = [req.params.startdate, req.params.enddate];
 
     const rows = await db.all(sqlQuery, params, (err, rows) => {
       if (err) {
@@ -95,17 +97,16 @@ router.get("/getmonth/:year/:month", async function (req, res) {
 // [x]
 router.get("/getyear/:year", async function (req, res) {
   try {
-    console.log(req.params.year);
-    const param = req.params.year;
-    const sqlQuery = `SELECT strftime('%m', created_at) AS created_at,
-            emotion_id,
-            sub_emotion_id,
-            COUNT(*) AS count
-            FROM emotions
-            WHERE CAST(strftime('%Y', created_at)as INTEGER) = ?
-            GROUP BY strftime('%m', created_at), emotion_id, sub_emotion_id`;
-
-    const params = [req.params.year];
+    const year = req.params.year;
+    const sqlQuery =
+        "SELECT strftime('%m', created_at) AS created_at,\n" +
+        "emotion_id,\n" +
+        "sub_emotion_id,\n" +
+        "COUNT(*) AS count\n" +
+        "FROM emotions\n" +
+        "WHERE CAST(strftime('%Y', created_at)as INTEGER) = ?\n" +
+        "GROUP BY strftime('%m', created_at), emotion_id, sub_emotion_id;";
+    const params = [year];
 
     const rows = await db.all(sqlQuery, params, (err, rows) => {
       if (err) {
