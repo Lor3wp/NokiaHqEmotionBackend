7ca35d3d5d28254f7c1625041d2ac7970aebd02f
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2023-05-03 13:15:10 +0300

    moi

--- NodeRest/routes/emotionsRoute.js
+++ NodeRest/routes/emotionsRoute.js
@@ -383,7 +383,7 @@ router.post("/addemotion", async function (req, res) {
       "INSERT INTO emotions (emotion_id, sub_emotion_id) VALUES (?, ?)";
     const result = await db.run(sqlQuery, [emotion, subEmotion]);
     res.status(200).json({ emotionId: result.insertID });
-    await tasker();
+    await taskerTablet(colors[emotion - 1]);
   } catch (error) {
     res.status(400).send(error.message);
   }
